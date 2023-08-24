85e3238cf5a0445ddf67edb1dbb2ed828e9190b0
Eduardo Ramos <exr4020@mavs.uta.edu>
2023-08-24 10:01:35 -0500

    P01: Extreme bonus part2

--- P01/extreme_bonus/Hello.java
+++ P01/extreme_bonus/Hello.java
@@ -2,7 +2,11 @@ public class Hello
 {
   public static void main (String [] args)
   {
-     
+    String username = System.getProperty("user.name");
+
+    System.out.print("Hello, " + username + "!");
+
+
   }
 
 }
