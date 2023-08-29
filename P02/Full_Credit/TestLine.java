0e6f8858eadfb013b9041e868346286b38661c36
Eduardo Ramos <exr4020@mavs.uta.edu>
2023-08-29 18:54:11 -0500

    P02:Full_Credit P01

---
+++ P02/Full_Credit/TestLine.java
@@ -0,0 +1,11 @@
+public class TestLine
+{
+  public static void main (String [] args)
+  {
+      Line point = new Line(2.32, 3.25, 1, 0 , Color.BLOND);
+
+      System.out.println(point + point.Length);
+
+
+  }
+}
