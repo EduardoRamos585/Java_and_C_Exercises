9c79fcd979ac162e0d96314fd219c282409ac360
Eduardo Ramos <exr4020@mavs.uta.edu>
2023-09-05 20:57:15 -0500

    P03: Full_Credit P1

--- P02/Full_Credit/Color.java
+++ P02/Full_Credit/Color.java
@@ -1,4 +1,18 @@
-public enum Color { AUBURN, AZURE, BEIGE,
-		    BLOND
+public enum Color { AUBURN(0xA52A2A), AZURE(0x007FFF), BEIGE(0xF5F5DC),BLOND(0xFAF0BE);
+
+ private int rgb;
+
+ private Color(int rgb)
+ {
+   this.rgb = rgb;
+ }
+
+ @Override
+ public String toString()
+ {
+   return this.name() + " " + "(" + String.format("%x",rgb) + ")";
+   
+ }
+
 }
 
---
+++ P03/Publication.java
@@ -0,0 +1,30 @@
+public class Publication
+{
+   private String title;
+   private String author;
+   private int copyright;
+   private String loanedTo;
+   private LocalDate dueDate;
+
+   public Publication(String title, String author, int copyright)
+   {
+      this.title = title;
+      this.author = author;
+      this.copyright = copyright;
+
+   }
+
+   public checkOut( String patron)
+   {
+     
+   }
+
+   public checkIn()
+   {
+
+   }
+
+
+
+}
+
