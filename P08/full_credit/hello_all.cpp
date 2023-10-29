c6c684261bcc88a8ac574f65f5d06c2d8444d490
Eduardo Ramos <exr4020@mavs.uta.edu>
2023-10-28 22:24:01 -0500

    P08:full_credit pt1

---
+++ P08/full_credit/hello_all.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main ()
+{
+  std::string name1;
+
+  std::cout << " What is your name ? "; 
+
+  std::cin >> name1;
+
+  std::cout << "Hello, " << name1 << std::endl;
+
+}
+
---
+++ P08/full_credit/hello_me.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+int main()
+{
+  std::cout << "Hello, Eduardo Ramos! " << std::endl;
+
+}
+
