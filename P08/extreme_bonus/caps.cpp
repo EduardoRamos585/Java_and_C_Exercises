cd2a4ff8f6d9ff048f3256a8705f7e413f45453e
Eduardo Ramos <exr4020@mavs.uta.edu>
2023-10-29 20:34:48 -0500

    P08:extreme_bonus pt1

--- P08/extreme_bonus/caps.cpp
+++ P08/extreme_bonus/caps.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-int main(int argc, char* argvs[])
+
+bool naturalOrder( std::string &a , std::string &b) {return a<b;}
+
+bool SortbyLength(std::string &a, std::string &b) {return a.length() == b.length() ? a<b : a.length() < b.length();}
+
+
+int main (int argc, char* argvs[])
 {
   std::vector<std::string> UpperCap;
   std::vector<std::string> *LowerCap = new std::vector<std::string>;
@@ -13,7 +20,7 @@ int main(int argc, char* argvs[])
     {
        if((argvs[i][0] > 64 ) && (argvs[i][0] < 91))
        {
-	  std::string a = argvs[i];
+          std::string a = argvs[i];
 	  UpperCap.push_back(a);
        }
        else
@@ -28,12 +35,16 @@ int main(int argc, char* argvs[])
 
   std::cout << "\nCapitalized " << std::endl;
 
+  std::sort(UpperCap.begin(), UpperCap.end(),naturalOrder);
+
   for(auto i : UpperCap)
   {
     std::cout << i << std::endl;
   }
 
   std::cout << "\nUncapitalized " << std::endl;
+
+  std::sort(LowerCap->begin(), LowerCap->end(), SortbyLength );
 
   for(auto v : *LowerCap)
   {
