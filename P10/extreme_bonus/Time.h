4ca014558812fcd20042c3907b249635b1f1dd59
Eduardo Ramos <exr4020@mavs.uta.edu>
2023-11-13 18:38:08 -0600

    P10 : Extreme_bonus pt1

---
+++ P10/extreme_bonus/Time.h
@@ -0,0 +1,42 @@
+#ifndef _TIME_H_
+#define _TIME_H_
+
+#include <iostream>
+
+
+class Time{
+
+	
+        private:
+        int _hour;
+        int _minute;
+        int _second;
+
+        void rationalize();
+
+
+
+
+	public:
+
+	Time(int hour, int minute, int second);
+	Time();
+	Time operator+(Time time);
+	Time operator+(int seconds) const;
+	friend Time operator+(int seconds, Time& time);
+	Time& operator++(); // Pre-Increment
+	Time operator++(int); // Post-Increment
+	int operator[](int index) const;
+	bool operator==(const Time& time);
+	bool operator!=(const Time& time);
+	bool operator<(const Time& time);
+	bool operator>(const Time& time);
+	bool operator<=(const Time& time);
+	bool operator>=(const Time& time);
+	friend std::ostream& operator<<(std::ostream& ost, const Time& time);
+	friend std::istream& operator>>(std::istream& ist,  Time& time);
+	
+
+};
+#endif
+
